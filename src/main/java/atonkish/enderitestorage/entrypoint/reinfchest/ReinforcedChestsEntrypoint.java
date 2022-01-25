ad2d2c66a6026ae4feb6a62d25ab18e4a1091ebf
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-01-25 20:52:52 +0900

    feat: add Enderite Chest

--- src/main/java/atonkish/enderitestorage/entrypoint/reinfchest/ReinforcedChestsEntrypoint.java
+++ src/main/java/atonkish/enderitestorage/entrypoint/reinfchest/ReinforcedChestsEntrypoint.java
@@ -1,9 +1,32 @@
 package atonkish.enderitestorage.entrypoint.reinfchest;
 
+import atonkish.reinfcore.util.ReinforcingMaterial;
 import atonkish.reinfchest.api.ReinforcedChestsModInitializer;
+import atonkish.reinfchest.api.ReinforcedChestsRegistry;
+import atonkish.enderitestorage.EnderiteReinforcedStorageMod;
+import atonkish.enderitestorage.util.ReinforcingMaterialSettings;
 
 public class ReinforcedChestsEntrypoint implements ReinforcedChestsModInitializer {
     @Override
     public void onInitializeReinforcedChests() {
+        initializeReinforcedChests();
+    }
+
+    private static void initializeReinforcedChests() {
+        for (ReinforcingMaterialSettings materialSettings : ReinforcingMaterialSettings.values()) {
+            ReinforcingMaterial material = materialSettings.getMaterial();
+
+            // Stats
+            ReinforcedChestsRegistry.registerMaterialOpenStat(EnderiteReinforcedStorageMod.MOD_ID, material);
+
+            // Blocks
+            ReinforcedChestsRegistry.registerMaterialBlock(EnderiteReinforcedStorageMod.MOD_ID, material,
+                    materialSettings.getBlockSettings());
+            ReinforcedChestsRegistry.registerMaterialBlockEntityType(EnderiteReinforcedStorageMod.MOD_ID, material);
+
+            // Items
+            ReinforcedChestsRegistry.registerMaterialItem(EnderiteReinforcedStorageMod.MOD_ID, material,
+                    materialSettings.getItemSettings());
+        }
     }
 }
