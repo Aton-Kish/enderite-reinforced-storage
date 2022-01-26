d5d208003fcad3084d9adf5939135fe2eca29236
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-01-26 22:48:09 +0900

    feat: add Enderite Barrel

--- src/main/java/atonkish/enderitestorage/entrypoint/reinfbarrel/ReinforcedBarrelsEntrypoint.java
+++ src/main/java/atonkish/enderitestorage/entrypoint/reinfbarrel/ReinforcedBarrelsEntrypoint.java
@@ -1,9 +1,32 @@
 package atonkish.enderitestorage.entrypoint.reinfbarrel;
 
+import atonkish.reinfcore.util.ReinforcingMaterial;
 import atonkish.reinfbarrel.api.ReinforcedBarrelsModInitializer;
+import atonkish.reinfbarrel.api.ReinforcedBarrelsRegistry;
+import atonkish.enderitestorage.EnderiteReinforcedStorageMod;
+import atonkish.enderitestorage.util.ReinforcingMaterialSettings;
 
 public class ReinforcedBarrelsEntrypoint implements ReinforcedBarrelsModInitializer {
     @Override
     public void onInitializeReinforcedBarrels() {
+        initializeReinforcedBarrels();
+    }
+
+    private static void initializeReinforcedBarrels() {
+        for (ReinforcingMaterialSettings materialSettings : ReinforcingMaterialSettings.values()) {
+            ReinforcingMaterial material = materialSettings.getMaterial();
+
+            // Stats
+            ReinforcedBarrelsRegistry.registerMaterialOpenStat(EnderiteReinforcedStorageMod.MOD_ID, material);
+
+            // Blocks
+            ReinforcedBarrelsRegistry.registerMaterialBlock(EnderiteReinforcedStorageMod.MOD_ID, material,
+                    materialSettings.getBlockSettings());
+            ReinforcedBarrelsRegistry.registerMaterialBlockEntityType(EnderiteReinforcedStorageMod.MOD_ID, material);
+
+            // Items
+            ReinforcedBarrelsRegistry.registerMaterialItem(
+                    EnderiteReinforcedStorageMod.MOD_ID, material, materialSettings.getItemSettings());
+        }
     }
 }
